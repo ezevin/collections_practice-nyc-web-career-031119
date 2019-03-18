053b0bd20ecf1781fc4352e83a35e223f92a4edf
Elizabeth Zevin <ehzevin@gmail.com>
2019-03-18 13:05:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort_by { |x| -x}
+end 
+
+def sort_array_char_count(arr)
+  arr.sort_by { |x| x.length}
+end 
+
+def swap_elements(arr)
+  arr[0], arr[1], arr[2] = arr [0], arr[2], arr[1]
+end
+
+def reverse_array(arr)
+  arr.reverse 
+end
+
+def kesha_maker(arr)
+  arr.each do |x|
+    x.split
+    binding.pry
+  end
+end
